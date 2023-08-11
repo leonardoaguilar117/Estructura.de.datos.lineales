f8fe0bf2aa46daf36c1673696c05ad2bb8724864
Leonardo Aguilar Martínez <leonardogrl18@gmail.com>
2023-08-10 20:45:22 -0600

    Nuevo material

---
+++ Codigos en clase/Clase09-08.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+/*Prototipos de funcion*/
+void buscar(int matricula);
+void insertar(int matricula[], int promedio[], string nombre[]);
+
+
+int main(){
+    int n, matricula[n],promedio[n];
+    string nombre[n];
+
+
+    cout<<nombre;
+
+
+
+
+    return 0;
+}
